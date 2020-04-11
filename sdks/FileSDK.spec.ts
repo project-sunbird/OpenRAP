ea52720d8b0a0164df66675c11cba758deb1f024
Vivek Kasture <vivek_kasture@techjoomla.com>
2020-04-11 21:02:51 +0530

    Issue #SB-17578 feat: Added test cases for FileSDK

--- sdks/FileSDK.spec.ts
+++ sdks/FileSDK.spec.ts
@@ -8,10 +8,11 @@ const expect = chai.expect;
 const data = 'test data'
 
 describe('FileSDK', () => {
-  let fileSDK;
+  let fileSDK, fileSDK1;
   process.env.FILES_PATH = path.join(__dirname, '..', 'test_data');
   beforeEach(async () => {
     fileSDK = new FileSDK("testPlugin");
+    fileSDK1 = new FileSDK("testPlugin");
   });
   afterEach(async () => {
     spy.restore();
@@ -20,16 +21,15 @@ describe('FileSDK', () => {
   process.env.FILES_PATH = path.join(__dirname, '..', 'test_data');
   it('should create test directory', (done) => {
     Promise
-      .all([fileSDK.mkdir('test1'), fileSDK.mkdir('test2')])
+      .all([fileSDK.mkdir('test1'), fileSDK.mkdir('test2', fileSDK1.getAbsPath(''))])
       .then(() => {
         let stats1 = fs.statSync(fileSDK.getAbsPath('test1'));
-        console.log(fileSDK.getAbsPath('test1'))
+        console.log(fileSDK.getAbsPath('test1'));
         expect(stats1.isDirectory()).to.be.true;
-        let stats2 = fs.statSync(fileSDK.getAbsPath('test2'));
+        let stats2 = fs.statSync(fileSDK1.getAbsPath('test2'));
         expect(stats2.isDirectory()).to.be.true;
         done();
       })
-
 
   });
 
@@ -52,8 +52,15 @@ describe('FileSDK', () => {
   })
 
   it('should remove file', async () => {
+
+    const pp = fileSDK.getAbsPath('test1');
+    fs.appendFileSync(path.join(pp, 'message.txt'), 'data to append');
     let Path = path.join('test1', 'moved.txt');
+    let Path1 = path.join('test1', 'message.txt');
     await fileSDK.remove(Path);
+    await fileSDK.remove('message.txt', path.join(pp));
+    expect(fs.existsSync(Path)).to.be.false;
+    expect(fs.existsSync(Path1)).to.be.false;
   })
 
   it('should zip file', async () => {
@@ -76,8 +83,9 @@ describe('FileSDK', () => {
   it('should unzip folder', async () => {
     let dest = 'test-folder';
     let src = 'test-folder.zip';
-    await fileSDK.unzip(src, dest, false);
-  })
+    const response = await fileSDK.unzip(src, dest, true, fileSDK.getAbsPath(''));
+    expect(response).to.be.equal(path.join(fileSDK.getAbsPath(''), 'test-folder', 'test-folder'));
+  });
 
   it('should watch folder', (done) => {
     let Path = fileSDK.getAbsPath('test-folder');
