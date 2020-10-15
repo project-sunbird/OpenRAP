81e1cdab5ff8b1d628b9b324e12140ffae1d0e1e
Nikunj Beladiya <nikunj.beladiya@tarento.com>
2020-10-15 10:17:47 +0530

    Issue #SB-20723 feat: Move to V2 APIs for auth token

--- dist/sdks/DeviceSDK.d.ts
+++ dist/sdks/DeviceSDK.d.ts
@@ -4,9 +4,13 @@ export default class DeviceSDK {
     private databaseSdk;
     private config;
     private apiKey;
+    private v1_api_path;
+    private v2_api_path;
     initialize(config: IConfig): void;
     register(): Promise<void>;
     getToken(deviceId?: string): Promise<string>;
+    getTokenFromFallBackURL(options: any, fallBackURL?: string): Promise<string>;
+    getBearerToken(options: any): Promise<string>;
 }
 export interface IConfig {
     key: string;
